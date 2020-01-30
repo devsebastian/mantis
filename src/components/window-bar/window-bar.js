c86ee900faff19beef2483a30730f917bd61df84
Dev Sebastian <devsebastian@gmail.com>
2020-01-31 00:16:33 +0530

    added custom  window-bar

--- src/components/window-bar/window-bar.js
+++ src/components/window-bar/window-bar.js
@@ -1,14 +1,34 @@
 import * as React from 'react'
 import './window-bar.css'
 import Tabs from '../tabs/tabs'
+import minimise from '../../assets/minimise.svg'
+import maximise from '../../assets/maximise.svg'
+import close from '../../assets/close.svg'
+const { remote } = window.require('electron')
+
+function closeWindow() {
+    var window = remote.BrowserWindow.getFocusedWindow();
+    window.close();
+}
+
+function minimizeWindow() {
+    var window = remote.BrowserWindow.getFocusedWindow();
+    window.minimize();
+}
+
+function maximizeWindow() {
+    var window = remote.BrowserWindow.getFocusedWindow();
+    window.isMaximized() ? window.unmaximize() : window.maximize();
+}
 
 function WindowBar(props) {
     return (
         <div className="window-bar">
-            <Tabs tabs={props.tabs}
-                activeTabIndex={props.activeTabIndex}
-                addTab={props.addTab}
-                setActiveTab={props.setActiveTab} />
+            <div className="window-bar-spacer"></div>
+            <img className="window-bar-icon window-bar-icon--normal" onClick={minimizeWindow} src={minimise} />
+            <img className="window-bar-icon window-bar-icon--normal" onClick={maximizeWindow} src={maximise} />
+            <img className="window-bar-icon window-bar-icon--close" onClick={closeWindow} src={close} />
+
         </div>
     )
 }
