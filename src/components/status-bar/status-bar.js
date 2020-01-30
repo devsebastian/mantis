b395d9358ba513f834202e53dfde0bc3d3eb4f64
Dev Sebastian <devsebastian@gmail.com>
2020-01-31 00:14:29 +0530

    added status-bar

---
+++ src/components/status-bar/status-bar.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import './status-bar.css'
+
+class StatusBar extends React.Component {
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div className="status-bar">
+                <b>MANTIS</b> | CODE
+            </div>
+        )
+    }
+}
+
+export default StatusBar
