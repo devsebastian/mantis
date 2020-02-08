5091327f4d46aec89f20e4cd7ad936412eaa5a6e
Dev Sebastian <devsebastian@gmail.com>
2020-02-09 01:25:59 +0530

    minor modifications

--- src/components/status-bar/status-bar.js
+++ src/components/status-bar/status-bar.js
@@ -11,14 +11,15 @@ class StatusBar extends React.Component {
             <div className="status-bar">
                 <div className="status-bar__block"> <b>MANTIS</b> | CODE</div>
                 <div className="status-bar__spacer"></div>
+
+                <div className="status-bar__block" >
+                    Spaces: 4
+                </div>
                 <div className="status-bar__block">
                     {"Ln " + this.props.options.line + ", Col " + this.props.options.ch}
                 </div>
                 <div className="status-bar__block" onClick={this.props.toggleTerminalVisibility}>
                     TERMINAL
-                </div>
-                <div className="status-bar__block" >
-                    Spaces: 4
                 </div>
             </div>
         )
