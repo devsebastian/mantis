3bdc9f53094a9d3d8dce79788225e29c85af47ac
Dev Sebastian <devsebastian@gmail.com>
2020-02-09 23:35:27 +0530

    added few more icons

--- src/assets/icons-generator.js
+++ src/assets/icons-generator.js
@@ -8,13 +8,14 @@ import file from './icons/file.svg'
 import pdf from './icons/pdf.svg'
 import word from './icons/word.svg'
 import document from './icons/document.svg'
+import folder from './icons/folder-dump.svg'
 
-export default function FileIcon({ type, cname, size}) {
-    return <img className={cname} src={getIcon(type)} height={size} width={size} height={size} />
+export default function FileIcon({ type, cname, size }) {
+    return <img className={cname} src={getIcon(type)} height={size} width={size} />
 }
 
-function getIcon(type){
-    switch(type){
+function getIcon(type) {
+    switch (type) {
         case 'c': return c; break;
         case 'cpp': return cpp; break;
         case 'py': return python; break;
@@ -22,6 +23,8 @@ function getIcon(type){
         case 'exe': return exe; break;
         case 'docx': return word; break;
         case 'txt': return document; break;
+        case 'pdf': return pdf; break;
+        case 'folder': return folder; break;
         default: return file;
     }
 }
